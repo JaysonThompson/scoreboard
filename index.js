5e2b96f93cc597261892db917c00fe3251eff96f
Jayson Thompson <jaysonthompson922@gmail.com>
2023-06-21 19:04:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
-let homeCount = 0
-let awayCount = 0
+let score = 0
 
-let homeScore = document.getElementById('home-score')
-let awayScore = document.getElementById('away-score')
+const homeScore = document.querySelector('#home-score')
+const homeAdd1Btn = document.querySelector('.home #add-one')
+const homeAdd2Btn = document.querySelector('.home #add-two')
+const homeAdd3Btn = document.querySelector('.home #add-three')
+const awayScore = document.getElementById('away-score')
 
-function homeAdd1() {
-  homeCount += 1
-  homeScore.textContent = homeCount
-}
-function homeAdd2() {
-  homeCount += 2
-  homeScore.textContent = homeCount
-}
-function homeAdd3() {
-  homeCount += 3
-  homeScore.textContent = homeCount
-}
-function awayAdd1() {
-  awayCount += 1
-  awayScore.textContent = awayCount
+const addPoints = numberOfPoints => {
+  if(numberOfPoints === 1) {
+    score += 1
+  } else if (numberOfPoints === 2){
+    score += 2
+  } else if (numberOfPoints === 3){
+    score += 3
+  }
 }
 
-function awayAdd2() {
-  awayCount += 2
-  awayScore.textContent = awayCount
-}
-function awayAdd3() {
-  awayCount += 3
-  awayScore.textContent = awayCount
-}
+
+console.log(homeAdd1Btn)
+
+// function homeAdd1() {
+//   homeCount += 1
+//   homeScore.textContent = homeCount
+// }
+// function homeAdd2() {
+//   homeCount += 2
+//   homeScore.textContent = homeCount
+// }
+// function homeAdd3() {
+//   homeCount += 3
+//   homeScore.textContent = homeCount
+// }
+// function awayAdd1() {
+//   awayCount += 1
+//   awayScore.textContent = awayCount
+// }
+
+// function awayAdd2() {
+//   awayCount += 2
+//   awayScore.textContent = awayCount
+// }
+// function awayAdd3() {
+//   awayCount += 3
+//   awayScore.textContent = awayCount
+// }
